34ccdf87bf4efdb0939117f0fd0bac7690c7b6f7
John Vehikite <johncookv@gmail.com>
2018-09-10 03:01:55 -0700

    Update url for 360 iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ export default () => (
     <Header />
     <Container>
       <h2 id="360-tour" className={styles.h2}>360 Tour</h2>
-      <p>Click and drag to look around. To zoom, use the scroll wheel on a physical mouse or (in Chrome) pinch to zoom on a touchpad.</p>
-      <iframe title="Outside Front" width="600" height="600" src="outside-front.html" frameBorder="0" allowFullScreen></iframe>
+      <p>Click and drag to look around. To zoom, use the scroll wheel on a physical mouse or two-finger scroll on the touchpad.</p>
+      <iframe title="Outside Front" width="600" height="600" src="http://terra-bella-outside-front.surge.sh/" frameBorder="0" allowFullScreen></iframe>
     </Container>
   </Fragment>
 )
